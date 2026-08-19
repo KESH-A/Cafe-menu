1fa89d37e35b22a861a2f735124fe06726d56e0a
KESH-A <kerempermanow2@gmail.com>
2026-08-19 15:30:30 +0500

    fix initial windowWidth state

--- src/App.jsx
+++ src/App.jsx
@@ -59,6 +59,7 @@ useEffect(() => {
   const handleResize = () => {
     setWindowWidth(window.innerWidth);
   };
+  handleResize();
   window.addEventListener("resize", handleResize);
   return () => window.removeEventListener("resize", handleResize);
 }, []);
